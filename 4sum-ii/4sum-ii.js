cf8dd5bcb2780810d65cc776c186b2db98d9d0d5
Yury Balduev <mesa5150@gmail.com>
2024-04-09 11:47:10 -0700

    Time: 163 ms, Memory: 55.2 MB - LeetHub

---
+++ 4sum-ii/4sum-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function(nums1, nums2, nums3, nums4) {
+    let count = 0;
+    let map = new Map();
+
+    for(let num1 of nums1) {
+        for(let num2 of nums2) {
+            map.set(num1 + num2, (map.get(num1 + num2) || 0) + 1)
+        }
+    }
+
+    for(let num3 of nums3) {
+        for(let num4 of nums4) {
+            count += map.get(-(num3 + num4)) || 0;
+        }
+    }
+
+    return count;
+};
