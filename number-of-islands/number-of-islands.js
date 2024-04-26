4eac30ceaeb55a002092ddee7cd37a11e408b466
Yury Balduev <mesa5150@gmail.com>
2024-04-26 11:30:37 -0700

    Time: 80 ms, Memory: 53.9 MB - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -10,6 +10,21 @@ const DIRS = [[1, 0], [-1, 0], [0, 1], [0, -1]];
 
 const inBound = (c, r, arr) => c >= 0 && c < arr.length && r >= 0 && r < arr[0].length;
 
+const dfs = (c, r, grid) => {
+    if (!inBound(c, r, grid) || grid[c][r] === WATER) {
+        return;
+    }
+
+    grid[c][r] = WATER;
+
+    for (let [cd, rd] of DIRS) {
+        const newC = c + cd;
+        const newR = r + rd;
+
+        dfs(newC, newR, grid);
+    }
+};
+
 var numIslands = function(grid) {
     const m = grid.length;
     const n = grid[0].length;
@@ -19,28 +34,7 @@ var numIslands = function(grid) {
         for(let row = 0; row < n; row++) {
             if (grid[col][row] === LAND) {
                 count++;
-                const queue = [[col, row]];
-
-                while(queue.length) {
-                    const [landCol, landRow] = queue.shift();
-
-                    if (grid[landCol][landRow] === WATER) {
-                        continue;
-                    }
-
-                    for(let [cd, rd] of DIRS) {
-                        const c = landCol + cd;
-                        const r = landRow + rd;
-
-                        if (!inBound(c, r, grid)) {
-                            continue;
-                        }
-
-                        queue.push([c, r])
-                    }
-
-                    grid[landCol][landRow] = WATER;
-                }
+                dfs(col, row, grid);
             }
         }
     }
