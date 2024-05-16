0fdc520bc66211c992de7341f2b51e8f481b598a
Yury Balduev <mesa5150@gmail.com>
2024-05-15 17:05:07 -0700

    Time: 94 ms, Memory: 58.5 MB - LeetHub

--- 0top-k-frequent-elements/0top-k-frequent-elements.js
+++ 0top-k-frequent-elements/0top-k-frequent-elements.js
@@ -4,23 +4,94 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
+    const comparator = (a, b) => a.count - b.count;
     const map = new Map();
-    const bucket = [];
-    const res = [];
+    const heap = new Heap([], comparator);
 
     for(let num of nums) {
-        map.set(num, (map.get(num) || 0) + 1);
+        map.set(num, (map.get(num) || 0 ) + 1);
     }
 
-    for(let [num, freq] of map) {
-        bucket[freq] = (bucket[freq] || new Set()).add(num)
+    for(let [num, count] of map) {
+        heap.add({ num, count })
+        if(heap.size() > k) heap.poll();
     }
 
-    for(let i = bucket.length - 1; res.length < k; i--) {
-        if (bucket[i]) {
-           res.push(...bucket[i]) 
+    const result = [];
+    while(heap.size() > 0) {
+        result.push(heap.poll().num);
+    }
+    return result;
+};
+
+class Heap {
+    constructor(data = [], comparator = (a, b) => a - b) {
+        this.data = data;
+        this.comparator = comparator;
+    }
+
+    add(val) {
+        this.data.push(val);
+        this.upBubble(this.size() - 1);
+    }
+
+    poll() {
+        if(this.size() === 0) return null;
+        const result = this.data[0];
+        const last = this.data.pop();
+        if(this.size() !== 0) {
+            this.data[0] = last;
+            this.downBubble(0);
+        }
+        return result;
+    }
+
+    upBubble(index) {
+        while(index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if(this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
+            } else {
+                break;
+            }
         }
     }
 
-    return res;
-};
+    downBubble(index) {
+        const lastIndex = this.size() - 1;
+        while(true) {
+            let leftIndex = index * 2 + 1;
+            let rightIndex = index * 2 + 2;
+            let findIndex = index;
+
+            if(leftIndex <= lastIndex && this.comparator(this.data[leftIndex], this.data[findIndex]) < 0) {
+                findIndex = leftIndex;
+            }
+
+            if(rightIndex <= lastIndex && this.comparator(this.data[rightIndex], this.data[findIndex]) < 0) {
+                findIndex = rightIndex;
+            }
+
+            if(findIndex !== index) {
+                this.swap(index, findIndex);
+                index = findIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    swap(idx1, idx2) {
+        [this.data[idx1], this.data[idx2]] = [this.data[idx2], this.data[idx1]]
+    }
+
+    peek() {
+        if(this.size() === 0) return null;
+        return this.data[0];
+    }
+
+    size() {
+        return this.data.length;
+    }
+}
