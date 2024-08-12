38e7e8063b195d594a9bb8fdc44e8a507355c574
Yury Balduyeu <mesa5150@gmail.com>
2024-08-12 12:30:17 -0700

    Time: 44 ms, Memory: 49.2 MB - LeetHub

---
+++ unique-paths-ii/unique-paths-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    const m = obstacleGrid.length;
+    const n = obstacleGrid[0].length;
+    if (obstacleGrid[0][0] === 1 || obstacleGrid[m - 1][n - 1] === 1) {
+        return 0;
+    }
+
+    const dp = new Array(m).fill(null).map(() => new Array(n).fill(0));
+    dp[0][0] = 1;
+
+    for(let i = 0; i < m; i++) {
+        for(let j = 0; j < n; j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                if (i > 0) {
+                   dp[i][j] = dp[i][j] + dp[i - 1][j];
+                }
+                if (j > 0) {
+                    dp[i][j] = dp[i][j] + dp[i][j - 1];
+                }
+            }
+        }
+    }
+
+    return dp[m - 1][n - 1];
+};
